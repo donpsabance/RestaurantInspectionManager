d502a2d07e2000580d5931522162bd38df2fd8a9
donpsabance <donpsabance@gmail.co>
2020-03-01 23:12:40 -0800

    set up list view

--- app/src/main/java/com/example/restaurantinspection/MainActivity.java
+++ app/src/main/java/com/example/restaurantinspection/MainActivity.java
@@ -2,12 +2,66 @@ package com.example.restaurantinspection;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.restaurantinspection.model.Restaurant;
+import com.example.restaurantinspection.model.RestaurantManager;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RestaurantManager restaurantManager = RestaurantManager.getInstance();
+
+    private class CustomListAdapter extends ArrayAdapter<Restaurant>{
+        public CustomListAdapter(){
+            super(MainActivity.this, R.layout.restaurantlistlayout, restaurantManager.getRestaurantList());
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup){
+
+            View itemView = view;
+            if(itemView == null){
+                itemView = getLayoutInflater().inflate(R.layout.restaurantlistlayout, viewGroup, false);
+            }
+
+            Restaurant restaurant = restaurantManager.getRestaurantList().get(position);
+
+            ImageView imageView = findViewById(R.id.restaurantIcon);
+            imageView.setImageResource(R.drawable.icon);
+
+            TextView textView = findViewById(R.id.restaurantDescription);
+            textView.setText(restaurant.toString());
+
+            return itemView;
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //temp
+        ArrayAdapter<Restaurant> adapter = new CustomListAdapter();
+        ListView listView = findViewById(R.id.restaurantListView);
+
+        listView.setAdapter(adapter);
+
     }
+
+    public void loadRestaurants(){
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/restaurantinspection/model/RestaurantManager.java
+++ app/src/main/java/com/example/restaurantinspection/model/RestaurantManager.java
@@ -1,4 +1,26 @@
 package com.example.restaurantinspection.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RestaurantManager {
+
+    private static RestaurantManager instance;
+    public static RestaurantManager getInstance(){
+        if(instance == null){
+            instance = new RestaurantManager();
+        }
+        return instance;
+    }
+
+    private List<Restaurant> restaurantList = new ArrayList<>();
+
+
+    public List<Restaurant> getRestaurantList() {
+        return restaurantList;
+    }
+
+    public void add(Restaurant restaurant){
+        restaurantList.add(restaurant);
+    }
 }
