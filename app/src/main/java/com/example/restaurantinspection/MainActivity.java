5d71b79192c735feadf32fa190c5e7f9c0f616b5
donpsabance <donpsabance@gmail.co>
2020-03-06 23:58:17 -0800

    sorted inspections by date

--- app/src/main/java/com/example/restaurantinspection/MainActivity.java
+++ app/src/main/java/com/example/restaurantinspection/MainActivity.java
@@ -123,9 +123,9 @@ public class MainActivity extends AppCompatActivity {
         if(dateDifference < 30){
             result = Long.toString(dateDifference);
         } else if (dateDifference > 30 && dateDifference < 365){
-            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH) - 1] + " " + calendar.get(Calendar.DAY_OF_MONTH);
+            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.DAY_OF_MONTH);
         } else {
-            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH) - 1] + " " + calendar.get(Calendar.YEAR);
+            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.YEAR);
         }
 
         return result;
