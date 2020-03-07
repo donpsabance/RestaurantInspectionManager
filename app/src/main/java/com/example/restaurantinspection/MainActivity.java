b7956624e1adf55c126fcd1e6d0e7ae19e7a811b
donpsabance <donpsabance@gmail.co>
2020-03-06 23:39:00 -0800

    sorted inspections by date

--- app/src/main/java/com/example/restaurantinspection/MainActivity.java
+++ app/src/main/java/com/example/restaurantinspection/MainActivity.java
@@ -114,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
         String result = "";
 
-/*
         Date dateToday = new Date();
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(inspectionDate);
@@ -124,13 +123,12 @@ public class MainActivity extends AppCompatActivity {
         if(dateDifference < 30){
             result = Long.toString(dateDifference);
         } else if (dateDifference > 30 && dateDifference < 365){
-            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH) - 1] + " " + calendar.get(Calendar.DAY_OF_MONTH);
+            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.DAY_OF_MONTH);
         } else {
-            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH) - 1] + " " + calendar.get(Calendar.YEAR);
+            result = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.YEAR);
             Log.wtf("DATE:", dateDifference + " ");
             Log.wtf("DATE:", "  " + calendar.get(Calendar.MONTH) + "  " +  calendar.getTime());
         }
-*/
 
         return result;
 
@@ -164,9 +162,9 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             restaurantManager.getRestaurantList().sort(new RestaurantComparator());
-//            for(Restaurant restaurant : restaurantManager){
-//                Collections.sort(restaurant.getInspectionManager().getInspectionList(),new InspectionComparator());
-//            }
+            for(Restaurant restaurant : restaurantManager){
+                Collections.sort(restaurant.getRestaurantInspectionList(),new InspectionComparator());
+            }
         }
 
 
