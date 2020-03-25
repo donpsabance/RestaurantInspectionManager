8e381347fded13b969138c675b087d1a405b9f37
feifanw <feifan_wang@sfu.ca>
2020-03-24 17:31:06 -0700

    DownloadingDialog

--- app/src/main/java/com/example/restaurantinspection/RequireDownloadActivity.java
+++ app/src/main/java/com/example/restaurantinspection/RequireDownloadActivity.java
@@ -2,7 +2,9 @@ package com.example.restaurantinspection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -66,7 +69,7 @@ public class RequireDownloadActivity extends AppCompatActivity {
         registerClickCallback();
         check_For_Updates(ID_RESTAURANTS);
 
-//        justLoadWhateverInStorage();
+        //justLoadWhateverInStorage();
     }
 
     private void justLoadWhateverInStorage() {
@@ -86,10 +89,26 @@ public class RequireDownloadActivity extends AppCompatActivity {
         }.execute();
     }
 
+    private void DownloadingDialog()
+    {
+        ProgressDialog progressDialog = new ProgressDialog(RequireDownloadActivity.this);
+        progressDialog.setTitle("Please Wait~~");
+        progressDialog.setMessage("Downloading");
+        progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //
+            }
+        });
+        progressDialog.show();
+    }
+
+
 
 
     private void registerClickCallback() {
         btnStartDownload.setOnClickListener(v -> {
+            DownloadingDialog();
             fetchPackages(ID_RESTAURANTS);
         });
 
