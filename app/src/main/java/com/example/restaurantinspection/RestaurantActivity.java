5b92fe0abe34266649133bd6dc4f7b8723e33017
Melody Lan <melody_lan@sfu.ca>
2020-03-30 12:24:30 -0700

    Add icons for marking favorite restaurants

--- app/src/main/java/com/example/restaurantinspection/RestaurantActivity.java
+++ app/src/main/java/com/example/restaurantinspection/RestaurantActivity.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
